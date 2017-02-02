14305ed12e4e6c6ee7555d7fa1e234e7f89ec1b3
megseoh <ohara.meagan@gmail.com>
2017-02-02 13:28:40 -0500

    adding form functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import ArtistContainer from './containers/artist-container.js';
+import logo from './logo.png';
+import ArtistContainer from './containers/similar-artists-container.js';
 import './App.css';
 
 class App extends Component {
@@ -9,10 +9,9 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Find similar artists to the ones you love.
         </p>
         <ArtistContainer />
       </div>
--- src/components/artist.js
+++ src/components/artist.js
@@ -4,20 +4,6 @@ export default function Artist({
 	artist
 }) {
 	return (
-		<div>
-			<h3>{ artist.name }</h3>
-			<img src={ artist.image } alt={ artist.name } />
-		</div>
+		<h1>{ artist}</h1>
 	);
 }
-
-
-
-// const Artist = (props) => (
-// 	<div>
-// 		<h3>{ props.name }</h3>
-// 		<img src={ props.image } alt={ props.name } />
-// 	</div>
-// );
-
-// export default Artist;

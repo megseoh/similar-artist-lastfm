e4554b2bf3012968c3b4180fc716ec8b045a0e6d
megseoh <ohara.meagan@gmail.com>
2017-02-10 19:52:45 -0500

    refactored app, now working

--- src/containers/similar-artists-container.js
+++ src/containers/similar-artists-container.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
 import Artist from '../components/artist.js';
+import ArtistForm from '../components/artist-form.js';
 import SimilarArtists from '../components/similar-artists.js';
+import SimilarArtistsURL from '../consts/similar-artists-url.jsx';
 import './similar-artists-container.css';
 
 const formInitialState = {
@@ -13,46 +15,58 @@ export default class ArtistContainer extends Component {
 		super(props);
 
 		this.state = {
-			artist: 'Cher',
+			currentArtist: 'Cher',
 			artistList: [],
 			form: formInitialState,
 		}
+
+		this.handleSearch = this.handleSearch.bind(this);
 	}
 
-	SIMILAR_ARTIST_URL = 'http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&limit=3&api_key=322a57c22e2af082f475e780809593d4&format=json&artist=cher';
+	SIMILAR_ARTIST_URL = 'http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&limit=3&api_key=322a57c22e2af082f475e780809593d4&format=json';
 
 	componentDidMount() {
-		$.get(this.SIMILAR_ARTIST_URL).then(res => {
-			var artistArray = res.similarartists.artist;
-			console.log(artistArray);
+		let encodedArtist = encodeURIComponent(this.state.currentArtist);
+		// console.log(encodedArtist);
+		let requestURL = `${this.SIMILAR_ARTIST_URL}&artist=${encodedArtist}`;
+		// console.log(requestURL);
+
+		$.get(requestURL).then(res => {
+			// console.log(this.state.currentArtist);
+			var similarArtistArray = res.similarartists.artist;
+			// console.log(similarArtistArray);
 			this.setState({
-				artistList: artistArray,
+				artistList: similarArtistArray,
 			});
 		});
 	}
 
-
+	handleSearch(newArtist) {
+		console.log(newArtist + ' inside handleSearch');
+		SimilarArtistsURL.getArtists(newArtist).then(similarArtistArray => {
+			this.setState({
+				currentArtist: newArtist,
+				artistList: similarArtistArray
+			})
+		}, errorMessage => {
+			alert(`Sorry we can't find that artist, please search for another`);
+		});
+	}
 
 	render() {
 		return (
 			<div className="container">
 				<div className="row">
 					<h3>If you love...</h3>
-					<Artist artist={ this.state.artist } />
-			  	<form onSubmit={ this.handleSubmit } >
-			  		<label>
-			  			Enter artist's name:
-				  		<input type="text" value={ this.state.form.name } onChange={ this.handleChange } placeholder="eg. Cher" />
-			  		</label>
-			  		<input type="submit" value="Submit" />
-		  		</form>
+					<Artist artist={ this.state.currentArtist } />
+					<ArtistForm onSearch={ this.handleSearch } />
 					<h3>You should try...</h3>
 					<div className="artist-container">
-					{this.state.artistList.map(artistInfo => (
+					{ this.state.artistList.map(artistInfo => (
 						<SimilarArtists
-							key={artistInfo.name}
-							name={artistInfo.name}
-							image={artistInfo.image.find(imageData => imageData.size === 'extralarge')['#text']}
+							key={ artistInfo.name }
+							name={ artistInfo.name }
+							image={ artistInfo.image.find(imageData => imageData.size === 'extralarge')['#text'] }
 						/>))}
 					</div>
 				</div>

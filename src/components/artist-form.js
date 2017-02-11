f1821a4120403707f19b87e32f066562ce1f95bd
megseoh <ohara.meagan@gmail.com>
2017-02-10 19:51:19 -0500

    separated form into own component

---
+++ src/components/artist-form.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+export default class ArtistForm extends Component {
+
+	handleSubmit(event) {
+		event.preventDefault();
+
+		console.log(this.refs.currentArtist.value);
+
+		let newArtist = this.refs.currentArtist.value;
+
+		if (newArtist.length > 0) {
+			// how do i clear this out without making it undefined??
+			// this.refs.currentArtist = '';
+			this.props.onSearch(newArtist);
+		}
+	}
+
+	render() {
+		return (
+			<div>
+		  	<form onSubmit={ this.handleSubmit.bind(this) } >
+		  		<label>
+		  			Enter artist's name:
+			  		<input type="text" ref="currentArtist" placeholder="eg. Cher" />
+		  		</label>
+		  		<input type="submit" value="Submit" />
+	  		</form>
+			</div>
+		);
+	}
+}
